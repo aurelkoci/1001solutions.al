40b8b335f8cfbc30e2bbb0de33f653ea7a3c2c2f
Aurel koci <aurelkoci@gmail.com>
2024-02-23 12:21:04 +0100

    add 503 under construction

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,6 @@
+@extends('errors::minimal')
+
+@section('title', __('Service Unavailable'))
+@section('code', '503')
+@section('message', __('Under construction'))
+@section('note' , 'Our website is under construction, but we are ready to go!')
---
+++ resources/views/errors/minimal.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html class="h-full">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>@yield('title')</title>
+</head>
+
+<body class="h-full">
+    <main class="grid min-h-full px-6 py-24 bg-white place-items-center sm:py-32 lg:px-8">
+        <div class="text-center">
+
+            <p class="text-base font-semibold text-blue-600">
+                @yield('code')
+            </p>
+            <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-800 sm:text-5xl">
+                @yield('message')
+            </h1>
+            <div class="flex justify-center">
+                <img src="underconstruction.png" alt="Underconstruction">
+            </div>
+            <p class="mt-6 text-base leading-7 text-gray-600">
+                @yield('note')
+            </p>
+        </div>
+    </main>
+</body>
+
+</html>
